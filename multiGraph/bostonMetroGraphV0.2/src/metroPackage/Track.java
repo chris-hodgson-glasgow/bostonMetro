d87206c330069b6e8af15a73d92a7cadf94520bb
conrad1234 <gcb14183@uni.strath.ac.uk>
2016-09-30 14:17:51 +0100

    Added documentation to Track

--- multiGraph/bostonMetroGraphV0.2/src/metroPackage/Track.java
+++ multiGraph/bostonMetroGraphV0.2/src/metroPackage/Track.java
@@ -3,35 +3,69 @@ package metroPackage;
 public class Track {
     private Edge track;
 
+    /**
+     * Constructor setting up initial values
+     * @param id        id of track
+     * @param stationA  First station connected to this track
+     * @param stationB  Second station connected to this track
+     */
     public Track(int id, Station stationA, Station stationB){
         track = new GraphEdge(id, stationA, stationB);
     }
 
+    /**
+     * Gets the track object
+     * @return the track object
+     */
     public Edge getTrack(){
         return (track);
     }
 
+    /**
+     * Gets the track id
+     * @return the track id
+     */
     public int getId(){
         return (track.getId());
     }
 
+    /**
+     * Gets the first station connected to this track
+     * @return the first station connected to this track
+     */
     public Station getStationA(){
         return (track.getNodeA());
     }
 
+    /**
+     * Gets the second station connected to this track
+     * @return the second station connected to this track
+     */
     public Station getStationB(){
         return (track.getNodeB());
     }
 
+    /**
+     * Sets the id of this track
+     * @param id id to set
+     */
     public void setId(int id){
         track.setId(id);
     }
 
-    public void setStationA(Node stationA){
+    /**
+     * Sets the first station connected to this track
+     * @param stationA First station to set
+     */
+    public void setStationA(Station stationA){
         track.setNodeA(stationA);
     }
 
-    public void setStationB(Node stationB){
+    /**
+     * Sets the second station connected to this track
+     * @param stationB Second station to set
+     */
+    public void setStationB(Station stationB){
         track.setNodeB(stationB);
     }
 }
