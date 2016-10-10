9f991fa054a5075d086246e92cbf28dd71265548
Yukeung Michael Chan <emb14134@uni.strath.ac.uk>
2016-10-10 16:46:34 +0100

    updating track and IEdge

--- multiGraph/bostonMetroGraphV0.2/src/metroPackage/IEdge.java
+++ multiGraph/bostonMetroGraphV0.2/src/metroPackage/IEdge.java
@@ -6,35 +6,30 @@ public interface IEdge {
 	 * Gets the edge id.
 	 * @return the Edge id
 	 */
-	public String getlineName();
+	public String getLineName();
 
 	/**
 	 * Gets first adjacent node
 	 * @return The Node with matching id
 	 */
-	public INode getNodeA();
+	public int getNodeAId();
 
 	/**
 	 * Gets second adjacent node
 	 * @return The Node with matching id
 	 */
-	public 	INode getNodeB();
+	public 	int getNodeBId();
 
 	/**
 	 * Sets the id of this edge
 	 * @param id id to set
 	 */
-	public void setId(int id);
 
-	/**
-	 * Sets the first adjacent node on THIShis edge
-	 * @param nodeA Node to set as first adjacent node
-	 */
-	public void setNodeA(INode nodeA);
+	public void setNodeA(int nodeA);
 
 	/**
 	 * Sets the second adjacent node on this edge
 	 * @param nodeB Node to set as second adjacent node
 	 */
-	public void setNodeB(INode nodeB);
+	public void setNodeB(int nodeB);
 }
--- multiGraph/bostonMetroGraphV0.2/src/metroPackage/Track.java
+++ multiGraph/bostonMetroGraphV0.2/src/metroPackage/Track.java
@@ -5,50 +5,50 @@ public class Track implements IEdge{
 
 
 
-        private String lineName;
-        private int outboundID;
-        private int inboundID;
+    private String lineName;
+    private int outboundID;
+    private int inboundID;
 
 
 
 
 
-        Track(String name,int outboundID, int inboundID){
+    Track(String name,int outboundID, int inboundID){
 
-            this.lineName = name;
-            this.outboundID = outboundID;
-            this.inboundID = inboundID;
-
-        }
+        this.lineName = name;
+        this.outboundID = outboundID;
+        this.inboundID = inboundID;
 
 
-        public String getlineName() {
-            return lineName;
-
-
-
-        public INode getNodeA() {
-            return null;
-        }
-
-
-        public INode getNodeB() {
-            return null;
-        }
-
-        public void setId(int id) {
-
-        }
-
-
-        public void setNodeA(INode nodeA) {
-
-        }
-
-
-        public void setNodeB(INode nodeB) {
-
-        }
     }
 
+
+    public String getLineName() {
+        return lineName;
+    }
+
+
+
+    public int getNodeAId() {
+        return outboundID;
+    }
+
+
+    public int getNodeBId() {
+        return inboundID;
+    }
+
+
+
+
+    public void setNodeA(int nodeA) {
+        outboundID=nodeA;
+    }
+
+
+    public void setNodeB(int nodeB) {
+        inboundID=nodeB;
+    }
 }
+
+
