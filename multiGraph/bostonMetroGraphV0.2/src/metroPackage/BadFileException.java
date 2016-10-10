197253e0d8aae00ab45cd645462d29262108b0de
conrad1234 <gcb14183@uni.strath.ac.uk>
2016-10-10 17:31:01 +0100

    Removed unneeded graph

--- multiGraph/bostonMetroGraphV0.2/src/metroPackage/BadFileException.java
+++ multiGraph/bostonMetroGraphV0.2/src/metroPackage/BadFileException.java
@@ -1,12 +1,12 @@
 package metroPackage;
 
-public class BadFileException extends Exception {
+class BadFileException extends Exception {
 
     public BadFileException(){
         super();
     }
 
-    public BadFileException(String message){
+    BadFileException(String message){
         super(message);
     }
 	
